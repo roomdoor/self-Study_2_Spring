1b5215a3de635d9362bd6cfe714d38d2e7d5cbd9
roomdoor <roomdoor.lee@gmail.com>
2021-11-25 16:14:01 +0900

    11/25

--- Chapter05-04/src/main/java/com/example/chapter0504/aop/ParameterAop.java
+++ Chapter05-04/src/main/java/com/example/chapter0504/aop/ParameterAop.java
@@ -2,6 +2,7 @@ package com.example.chapter0504.aop;
 
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
@@ -22,10 +23,9 @@ public class ParameterAop {
     // 메소드 실행 전에 어떤 값이 들어가는지(before), 리턴할때 어떤 값이 리턴 되는지(afterReturning)
     @Before("cut()")   // pointcut 가 실행되는 메소드 넣어주면 pointCut 이전에 before 라는 메소드 실행시키겠다 라는 뜻
     public void before(JoinPoint joinPoint) { // JoinPoint 들어가는 지점에 대한 정보를 가진 객체
-        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        Method method = methodSignature.getMethod();
-        System.out.println(method.getName());
-
+        Signature signature = joinPoint.getSignature();
+        String method = signature.getName();
+        System.out.println(method);
 
         Object[] args = joinPoint.getArgs();    // 메소드에 들어가는 매개변수들의 배열
         for (Object obj : args) {
